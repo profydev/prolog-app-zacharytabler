d43f77a027cfa18ccbf88704074486fc31f29cb3
zacharytabler <74665162+zacharytabler@users.noreply.github.com>
2023-09-03 19:03:49 -0400

    Updated support button to open support email address and pre-fill subject line for user.

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -84,7 +84,10 @@ export function SidebarNavigation() {
               text="Support"
               iconSrc="/icons/support.svg"
               isCollapsed={isSidebarCollapsed}
-              onClick={() => alert("Support")}
+              onClick={() =>
+                (window.location.href =
+                  "mailto:support@prolog-app.com?subject=Support Request")
+              }
             />
             <MenuItemButton
               text="Collapse"
