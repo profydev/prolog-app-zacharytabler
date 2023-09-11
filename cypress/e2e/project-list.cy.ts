30f0a0f2599895334653d4ca36e8389128398c17
zacharytabler <74665162+zacharytabler@users.noreply.github.com>
2023-09-11 13:10:49 -0400

    Updated the text and badge color on the project card. Also added a cypress test to confirm changes were working. (#9)

--- cypress/e2e/project-list.cy.ts
+++ cypress/e2e/project-list.cy.ts
@@ -1,5 +1,6 @@
 import capitalize from "lodash/capitalize";
 import mockProjects from "../fixtures/projects.json";
+import { updateStatus } from "../../api/projects.types";
 
 describe("Project List", () => {
   beforeEach(() => {
@@ -32,7 +33,9 @@ describe("Project List", () => {
           cy.wrap($el).contains(languageNames[index]);
           cy.wrap($el).contains(mockProjects[index].numIssues);
           cy.wrap($el).contains(mockProjects[index].numEvents24h);
-          cy.wrap($el).contains(capitalize(mockProjects[index].status));
+          cy.wrap($el).contains(
+            capitalize(updateStatus(mockProjects[index].status)),
+          );
           cy.wrap($el)
             .find("a")
             .should("have.attr", "href", "/dashboard/issues");
